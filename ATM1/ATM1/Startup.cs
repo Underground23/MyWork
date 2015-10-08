3edef568bc39e5bd6ae8591b6c9d609378a40c25
Underground23 <dina23@hotmail.com>
2015-10-09 09:01:23 +1100

    Start Project

---
+++ ATM1/ATM1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ATM1.Startup))]
+namespace ATM1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
